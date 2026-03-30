16d4e732f694ca888db0d4de41392d767e387dc4
Nathan <209938737+quiteboring@users.noreply.github.com>
2026-03-30 16:59:35 -0400

    revert: switch mixin implementation from Java back to Kotlin

---
+++ src/main/kotlin/dev/lyzev/skia/mixin/MinecraftMixin.kt
@@ -0,0 +1,59 @@
+/*
+ * This file is part of https://github.com/Lyzev/Skija.
+ *
+ * Copyright (c) 2026. Lyzev
+ *
+ * Skija is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skija is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Skija. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.lyzev.skia.mixin
+
+import dev.lyzev.api.event.EventSkiaDraw
+import dev.lyzev.api.event.EventSkiaInit
+import dev.lyzev.api.skia.SkiaImplMc
+import dev.lyzev.skia.HudExample
+import net.minecraft.client.Minecraft
+import net.minecraft.client.main.GameConfig
+import org.lwjgl.glfw.GLFW
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+@Mixin(Minecraft::class)
+class MinecraftMixin {
+
+    @Inject(method = ["<init>"], at = [At("RETURN")])
+    private fun onInit(args: GameConfig?, ci: CallbackInfo?) {
+        val hudExample = HudExample
+        val instance = SkiaImplMc
+
+        val width = IntArray(1)
+        val height = IntArray(1)
+
+        GLFW.glfwGetFramebufferSize(Minecraft.getInstance().getWindow().handle(), width, height)
+        EventSkiaInit(if (width[0] > 0) width[0] else 1, if (height[0] > 0) height[0] else 1).fire()
+    }
+
+    @Inject(
+        method = ["renderFrame"],
+        at = [At(
+            value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/systems/RenderSystem;flipFrame(Lcom/mojang/blaze3d/TracyFrameCapture;)V"
+        )]
+    )
+    private fun onBeforeFlipFrame(advanceGameTime: Boolean, ci: CallbackInfo?) {
+        EventSkiaDraw.fire()
+    }
+
+}
--- src/main/kotlin/dev/lyzev/skia/mixin/WindowMixin.kt
+++ src/main/kotlin/dev/lyzev/skia/mixin/WindowMixin.kt
@@ -15,22 +15,21 @@
  *
  * You should have received a copy of the GNU General Public License along with Skija. If not, see <https://www.gnu.org/licenses/>.
  */
+package dev.lyzev.skia.mixin
 
-package dev.lyzev.skia.mixin;
+import com.mojang.blaze3d.platform.Window
+import dev.lyzev.api.event.EventSkiaInit
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
-import com.mojang.blaze3d.platform.Window;
-import dev.lyzev.api.event.EventSkiaInit;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+@Mixin(Window::class)
+class WindowMixin {
 
-@Mixin(Window.class)
-public class WindowMixin {
-
-    @Inject(method = "onFramebufferResize", at = @At("RETURN"))
-    private void onFramebufferResize(long handle, int width, int height, CallbackInfo ci) {
-        new EventSkiaInit(width > 0 ? width : 1, height > 0 ? height : 1).fire();
+    @Inject(method = ["onFramebufferResize"], at = [At("RETURN")])
+    private fun onFramebufferResize(handle: Long, width: Int, height: Int, ci: CallbackInfo?) {
+        EventSkiaInit(if (width > 0) width else 1, if (height > 0) height else 1).fire()
     }
 
 }
